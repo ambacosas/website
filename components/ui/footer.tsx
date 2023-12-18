ca3907952ce69b8e3c7887d5517d5dd8cdfc8396
LolCNFT <lolcnft@gmail.com>
2023-12-18 13:11:25 -0300

    edicion v.2

--- components/ui/footer.tsx
+++ components/ui/footer.tsx
@@ -112,14 +112,10 @@ export default function Footer() {
           <ul className="flex mb-4 md:order-1 md:ml-4 md:mb-0">
             <li>
               <a href="#0" className="flex p-2 hover:scale-105 justify-center items-center text-gray-600 hover:text-gray-900 bg-white hover:bg-white-100 rounded-full shadow transition duration-150 ease-in-out" aria-label="Linkedin">
-                <BsLinkedin className='hover:text-blue-500 w-4 h-4' />
+                <BsLinkedin className='hover:text-blue-500 w-6 h-6' />
               </a>
             </li>
-            <li>
-              <a href="#0" className="flex p-2 hover:scale-105 justify-center items-center text-gray-600 hover:text-gray-900 bg-white hover:bg-white-100 rounded-full shadow transition duration-150 ease-in-out" aria-label="Twitter">
-                <BsLinkedin className='hover:text-blue-500 w-4 h-4' />
-              </a>
-            </li>
+
             {/* <li className="ml-4">
               <a href="#0" className="flex justify-center items-center text-gray-600 hover:text-gray-900 bg-white hover:bg-white-100 rounded-full shadow transition duration-150 ease-in-out" aria-label="Github">
                 <svg className="w-8 h-8 fill-current" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg">
