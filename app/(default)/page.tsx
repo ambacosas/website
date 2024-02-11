da38fb8b8c427a57b1ca661960ee52c835103c90
lorenzobarrantes <lorenzobarrantes1@gmail.com>
2024-02-11 17:57:31 -0300

    favicon

--- app/(default)/page.tsx
+++ app/(default)/page.tsx
@@ -7,30 +7,24 @@ import Newsletter from '@/components/newsletter'
 import { LanguageProvider } from '@/components/context/LanguageContext';
 import ogLogo from '../../public/images/og.png';
 
-interface Metadata {
-  title: string;
-  description: string;
-  openGraph?: {
-    title: string;
-    description: string;
-    image: string;
-    url: string;
-    type: string;
-    site_name: string;
-  };
-}
 
-export const metadata: Metadata = {
+export const metadata = {
   title: 'AMBACO',
   description: '',
   openGraph: {
     title: 'AMBACO',
     description: '',
-    image: ogLogo.src,
     url: 'https://ambaco.com.ar/',
     type: 'website',
-    site_name: 'AMBACO'
-  }
+    siteName: 'AMBACO',
+    images: [
+      {
+        url: "http://drive.google.com/uc?export=view&id=1A-kn_UfSNzz8FwIuv_11-Gl7Pixq3vPJ",
+        width: 1600,
+        height: 630,
+      }
+    ]
+  },
 };
 
 export default function Home() {
